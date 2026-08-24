c020921ec2cce2ec64e189fce491e62a82fa5f58
Shivam Mishra <shivamrmdev@gmail.com>
2026-08-24 17:19:36 +0530

    Add: longest-common-prefix - submission-1

---
+++ Data Structures & Algorithms/longest-common-prefix/submission-1.js
@@ -0,0 +1,30 @@
+class Solution {
+    /**
+     * @param {string[]} strs
+     * @return {string}
+     */
+    longestCommonPrefix(strs) {
+        let refStr = strs[0];
+
+  let result = "";
+
+   if(strs.length === 1){
+    return refStr
+  }
+  for (let i = 0; i < refStr.length; i++) {
+    let found = false
+    for (let j = 1; j < strs.length; j++) {
+
+      if (refStr[i] !== strs[j][i]) {
+        return result;
+      }
+
+      found = true
+    }
+    if(found){
+        result += refStr[i]
+    }
+  }
+  return result;
+    }
+}
